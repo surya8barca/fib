2bf677293443bcd0cec9f2c95f5778fa8298c03c
Yogesh Agrawal <yogeshagrawal50@gmail.com>
2023-03-21 14:38:56 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 import './App.css';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import Sidenav from './layouts/sidenav/sidenav';
-import NavBar from './layouts/navbar/navbar';
+import NavBar from "./layouts/navbar/Navbar"
 
 function App() {
   return (
